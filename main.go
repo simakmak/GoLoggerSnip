4f13342ca8c8f958b8d4342a3d3c16f02c79fd23
Александр Симаков <a.simakov@rambler-co.ru>
2021-02-09 13:50:13 +0300

    Прикрутил нормальное логгирование FIX

--- main.go
+++ main.go
@@ -8,6 +8,15 @@ import (
 	"strings"
 )
 
+type Logger interface {
+	Debug(m interface{})
+	Debugf(m string, v ...interface{})
+	Error(m interface{})
+	Errorf(m string, v ...interface{})
+	Info(m interface{})
+	Infof(m string, v ...interface{})
+}
+
 type log struct {
 	logrus.Logger
 }
@@ -41,7 +50,11 @@ func LogHandler(lvl string) log {
 
 }
 
-func (l log) Log() *logrus.Entry {
+func (l log) Debug(m interface{}) {
+
+	l.Logger.Debug(m)
+}
+func (l log) Debugf(m string, v ...interface{}) {
 	pc, file, line, ok := runtime.Caller(1)
 	if !ok {
 		panic("Could not get context info for logger!")
@@ -50,12 +63,40 @@ func (l log) Log() *logrus.Entry {
 	filename := file[strings.LastIndex(file, "/")+1:] + ":" + strconv.Itoa(line)
 	funcname := runtime.FuncForPC(pc).Name()
 	fn := funcname[strings.LastIndex(funcname, ".")+1:]
-	return l.WithField("file", filename).WithField("function", fn)
-
+	l.WithField("file", filename).WithField("function", fn).Debugf(m, v)
 }
 
-func main() {
-	l := LogHandler("Debug")
+func (l log) Error(m interface{}) {
+	l.Logger.Error(m)
+}
 
-	l.Log().Debug("test")
+func (l log) Errorf(m string, v ...interface{}) {
+	pc, file, line, ok := runtime.Caller(1)
+	if !ok {
+		panic("Could not get context info for logger!")
+	}
+
+	filename := file[strings.LastIndex(file, "/")+1:] + ":" + strconv.Itoa(line)
+	funcname := runtime.FuncForPC(pc).Name()
+	fn := funcname[strings.LastIndex(funcname, ".")+1:]
+	l.WithField("file", filename).WithField("function", fn).Errorf(m, v)
+}
+
+func (l log) Info(m interface{}) {
+	l.Logger.Info(m)
+}
+
+func (l log) Infof(m string, v ...interface{}) {
+	l.Infof(m, v)
+}
+
+
+
+func main() {
+	var l Logger = LogHandler("Debug")
+
+	//l.Info("test")
+
+	l.Debugf("log %s", "test", "test2")
+	l.Info( "test")
 }
